08ac792d92ceaaa751803e0a5c64cfdfa1213ba0
philipAvdey <91579036+philipAvdey@users.noreply.github.com>
2024-06-01 15:31:08 -0700

    Added the Word component

--- src/components/HangmanWord/HangmanWord.tsx
+++ src/components/HangmanWord/HangmanWord.tsx
@@ -1,5 +1,23 @@
+import "./HangmanWord.scss";
+
+const word = "test";
+const guessedLetters = [""];
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  return (
+    <div className="app__word">
+      {word.split("").map((letter, index) => (
+        <span key={`${letter}-${index}`} className={`app__word_letter`}>
+          <span
+            className={`app__word_letter_${
+              guessedLetters.includes(letter) ? "visible" : "invisible"
+            }`}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
