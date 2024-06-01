9a0ff26617d6dbc1bcaba7f733ce81c20b1bbce5
philipAvdey <91579036+philipAvdey@users.noreply.github.com>
2024-06-01 13:36:30 -0700

    Added the body, legs, and arm

--- src/components/HangmanDrawing/HangmanDrawing.tsx
+++ src/components/HangmanDrawing/HangmanDrawing.tsx
@@ -1,12 +1,20 @@
 import "./HangmanDrawing.scss";
-const HEAD = (
-  <div className="app__drawing_head"></div>
-);
+const HEAD = <div className="app__drawing_head"></div>;
+const BODY = <div className="app__drawing_body"></div>;
+const RIGHT_ARM = <div className="app__drawing_right-arm"></div>;
+const LEFT_ARM = <div className="app__drawing_left-arm"></div>;
+const RIGHT_LEG = <div className="app__drawing_right-leg"></div>;
+const LEFT_LEG = <div className="app__drawing_left-leg"></div>;
 
 const HangmanDrawing = () => {
   return (
     <div className="app__drawing">
       {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
       <div className="app__drawing_noose-holder"></div>
       <div className="app__drawing_overhang"></div>
       <div className="app__drawing_vertical-section"></div>
